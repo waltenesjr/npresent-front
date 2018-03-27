d99ccb1c4ee4277ee5a82854035e995e23527516
waltenes-ps <waltenesjr@live.com>
2018-03-27 15:03:34 -0300

    Implementação do form de evento

--- src/app/pages/evento/evento.module.ts
+++ src/app/pages/evento/evento.module.ts
@@ -1,10 +1,8 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 
 import {EventoRoutingModule} from './evento-routing.module';
 import {EventoListComponent} from './evento-list/evento-list.component';
 import {EventoFormComponent} from './evento-form/evento-form.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CoreModule} from '../../@core/core.module';
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {PagesRoutingModule} from './pages-routing.module';
-import {EventoListComponent} from "./evento/evento-list/evento-list.component";
 import {CoreModule} from "../@core/core.module";
-import { HomeComponent } from './home/home.component';
+import {HomeComponent} from './home/home.component';
 
 @NgModule({
   imports: [
